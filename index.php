c2690214b48b17a359cca2c0bf2ed13077175b25
Matthew Agius <mtagius@gmail.com>
2018-05-03 15:50:18 -0400

    added credit text and twitter button

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
         <script src="jquery.jscroll.js"></script>
         <script type="text/javascript" src="main.js"></script>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </head>
     
     <body>
@@ -24,6 +25,11 @@
             <div id="main-title">
                 <img src="endless.png" alt="Endless" height="162" width="552">
                 <img src="donuts.png" alt="Donuts" height="162" width="547">
+            </div>
+
+            <div id="menu">
+                <div id="credit">Made by <a href="http://mtagius.com" target="new">Matt Agius</a></div>
+                <a class="twitter-share-button" href="https://twitter.com/intent/tweet?text=Check%20out%20this%20fun%20site%20with%20unlimited%20donuts!">Tweet</a>
             </div>
             
             <div id="donuts">
